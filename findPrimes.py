c6ddaab278fdc4a5e33a1fb966a836f2c042ce2f
Steve Sloan <ssloan@hospnet.ca>
2017-06-03 09:04:11 -0400

    trying to add threading

--- findPrimes.py
+++ findPrimes.py
@@ -1,4 +1,12 @@
 import math
+import sys
+
+import _thread
+import time
+
+
+allNumbers = []
+primes = []
 
 def is_prime(n):
     for i in range(3, n):
@@ -6,5 +14,26 @@ def is_prime(n):
             return False
     return True
 
-print(list(x for x in range(1000001) if is_prime(x)))
+def find_primes(count):
+    print(list(x for x in range(count) if is_prime(x)))
 
+def test():
+    global allNumbers
+    global primes
+    while allNumbers.length > 1:
+        number = allNumbers.pop()
+        if(is_prime(number)):
+            primes.append(number)
+
+def find_primes2(count):
+    global allNumbers
+    allNumbers = list(range(count))
+    try:
+        _thread.start_new_thread(test)
+    except:
+        print('cant do it')
+
+
+if __name__ == '__main__':
+    find_primes2(int(sys.argv[1]))
+    # print(allNumbers)
